84d1d6eb6703ec3592a64ccfea4207ad84a12e6f
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-22 22:44:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do 
     @operation = params[:operation]
     @num1 = params[:number1].to_i 
-    @num2 = params[:num2].to_i 
+    @num2 = params[:number2].to_i 
     
     case @operation
     when "add"
