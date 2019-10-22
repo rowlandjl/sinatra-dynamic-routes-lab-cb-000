42fbfab096568f573957df1cc2ede6d1707c594d
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-22 22:30:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do 
     @repeated_phrase = params[:number].to_i * params[:phrase]
-    " #{@repeated_phrase}"
+    "#{@repeated_phrase}"
   end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
