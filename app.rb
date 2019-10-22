99661741390e393bc4d76b1809518610ea231d38
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-22 22:35:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,6 +21,7 @@ class App < Sinatra::Base
   end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    "#{params[:word1] params[:word2]}"
   end 
   
   get '/:operation/:number1/:number2' do 
